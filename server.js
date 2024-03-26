cef4b2d2a7e5f3711eb2d36caeb65655872735f7
Jirinmas Siripakornchai <naniko.study@gmail.com>
2024-03-26 14:17:50 +0700

    fix CORS

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 const cors = require("cors");
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: 'https://swdevprac2-project-cha.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(helmet());
